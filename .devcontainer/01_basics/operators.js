35a1e5af1646df3b8f44371b3b7ba5eabb0a59c9
khushalhiranii <140611208+khushalhiranii@users.noreply.github.com>
2024-01-21 11:29:30 +0000

    number functions

--- .devcontainer/01_basics/operators.js
+++ .devcontainer/01_basics/operators.js
@@ -1,3 +1,9 @@
 console.log("1"+2+2)
 console.log(2+2+"1");
 console.log(2+"1"+2);
+
+console.log( null > 0)
+console.log( null == 0 );
+console.log( null >= 0 );
+// The reason is that an equality check == and comparisons >,<,>=,<= work differently
+// comparison converts null to number i.e. in line umber 7 null=0, therefore null>=0 => true
