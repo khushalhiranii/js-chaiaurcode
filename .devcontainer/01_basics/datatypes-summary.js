3f9cfaa80e1ad637fee537921261226dfef06db7
khushalhiranii <140611208+khushalhiranii@users.noreply.github.com>
2024-01-24 20:51:21 +0000

    modified them

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+//  primitive Datatype
+// 7 types : String, Number, Boolean, null, undefined, symbol,
+//           BigInt
+
+// Reference (Non primitive)
+// Types: Array, Objects, Functions
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
