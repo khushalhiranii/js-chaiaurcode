8aefb00a019dfee37fe9ef937e41d9547bdc398a
khushalhiranii <140611208+khushalhiranii@users.noreply.github.com>
2024-02-04 20:34:53 +0000

    background colour changer

---
+++ 04_projects/solution1.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (eve) {
+    console.log(eve);
+    console.log(eve.target);
+    
+    // if(eve.target.id === 'grey'){
+    //   body.style.backgroundColor = 'grey'
+    // }
+    // if(eve.target.id === 'white'){
+    //   body.style.backgroundColor = 'white'
+    // }
+    // if(eve.target.id === 'yellow'){
+    //   body.style.backgroundColor = 'yellow'
+    // }
+    // if(eve.target.id === 'blue'){
+    //   body.style.backgroundColor = 'blue'
+    // }
+    body.style.backgroundColor = eve.target.id
+  });
+});
